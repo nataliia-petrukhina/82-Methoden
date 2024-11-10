1983906688a12ca9a770c6acac93aa1f6bdfe1fb
nataliia_petrukhina <natali.insajt@gmail.com>
2024-11-10 21:11:50 +0100

    hhhh

---
+++ task-01.js
@@ -0,0 +1,9 @@
+/**
+ * Task 1
+ * 
+ * Fix the code below so that the array animals is printed to the console
+ */
+
+const animals = ["giraffe", "cat", "dog", "mouse", "koala"];
+
+console.log(animals);
---
+++ task-03.js
@@ -0,0 +1,15 @@
+/**
+ * Task 03
+ * 
+ * Complete the code below so that:
+ * 
+ * - the first console.log() outputs London
+ * - the second console.log() outputs Berlin
+ * 
+ */
+
+const cities = ["Berlin", "Leipzig", "London", "Rome", "Tokyo"];
+
+
+console.log(cities[2]); // London
+console.log(cities[0]); // Berlin 
